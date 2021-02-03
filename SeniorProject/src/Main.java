efa058f71019a21d0592b633a2666350bafce444
Pleonardo1 <pleonardo80@yahoo.com>
2021-02-03 17:54:04 -0500

    Update

--- SeniorProject/src/Main.java
+++ SeniorProject/src/Main.java
@@ -1,15 +1,15 @@
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
-
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        CharStream cs = CharStreams.fromFileName("./src/HelloWorld.Java");
+        CharStream cs = CharStreams.fromFileName("./src/MyNum.Java");
         JavaLexer javaLexer = new JavaLexer(cs);
         CommonTokenStream token = new CommonTokenStream(javaLexer);
         JavaParser parser = new JavaParser(token);
@@ -18,7 +18,14 @@ public class Main {
         MyVisitor visitor = new MyVisitor();
         visitor.visit(tree);
 
+        //Prints each token classifier and literal
+        for (Token t : token.getTokens()) {
+            System.out.printf("%-20s %s\n", JavaLexer.VOCABULARY.getSymbolicName(t.getType()), t.getText());
+        }
+
+        // Prints leaf nodes of the parse tree
         System.out.println(tree.getText());
 
     }
+
 }
---
+++ SeniorProject/src/MyNum.java
@@ -0,0 +1,6 @@
+public class MyNum {
+
+    public static void main(String[] args) {
+        int num = 5;
+    }
+}

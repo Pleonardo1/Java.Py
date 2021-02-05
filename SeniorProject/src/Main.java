fe5143fb32b20b11f7f452c1806d67dd10119a3d
Pleonardo1 <pleonardo80@yahoo.com>
2021-02-04 19:03:34 -0500

    Update

--- SeniorProject/src/Main.java
+++ SeniorProject/src/Main.java
@@ -4,14 +4,16 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        //CharStream cs = CharStreams.fromFileName("./src/MyNum.Java");
-        CharStream cs = CharStreams.fromFileName("./test.txt");
+        CharStream cs = CharStreams.fromFileName("./src/MyNum.Java");
+        //CharStream cs = CharStreams.fromFileName("./test.txt");
         JavaLexer javaLexer = new JavaLexer(cs);
         CommonTokenStream token = new CommonTokenStream(javaLexer);
         JavaParser parser = new JavaParser(token);
@@ -20,121 +22,25 @@ public class Main {
         MyVisitor visitor = new MyVisitor();
         visitor.visit(tree);
 
-        //Prints each token classifier and literal
-        /*for (Token t : token.getTokens()) {
-            System.out.printf("%-20s %s\n", JavaLexer.VOCABULARY.getSymbolicName(t.getType()), t.getText());
-        } */
+        List<String> myTokens = new ArrayList<>();
+        List<String> myLiterals = new ArrayList<>();
 
-        /*String[] pycode = transpile(token);
+        //Prints each token classifier and literal
+        for (Token t : token.getTokens()) {
+            myTokens.add(JavaLexer.VOCABULARY.getSymbolicName(t.getType()));
+            myLiterals.add(t.getText());
+        }
+
+        Transpile pycode = new Transpile(myTokens, myLiterals);
+
+        List<String> literals = pycode.transpiler();
+        System.out.println();
 
         // Need to find someway to account for spacing
-        for (int i = 0; i < pycode.length; i++) {
-            System.out.print(pycode[i]);
-        } */
-
-    }
-
-    public static String[] transpile(CommonTokenStream myTokens) {
-
-        //Array of the token vocab terms
-        String[] tokens = new String[myTokens.getNumberOfOnChannelTokens()];
-        //Array of parsed literals
-        String[] literals = new String[tokens.length];
-
-        // # of tokens
-        int streamLength = myTokens.getNumberOfOnChannelTokens();
-        int i = 0;
-
-        // Initializes values for the tokens and literals array
-        for (Token t : myTokens.getTokens()) {
-            tokens[i] = JavaLexer.VOCABULARY.getSymbolicName(t.getType());
-            literals[i] = t.getText();
-            i++;
+        for (int i = 0; i < literals.size(); i++) {
+            System.out.print(literals.get(i));
         }
 
-        // Trial transpilation of the literals using the vocab term
-        for (int j = 0; j < tokens.length; j++) {
-            //Left Brace counter for indentation
-            int numOfLbrace = 0;
-            String[] mainMethod = {"public", "static", "void", "main",
-                                    "(", "String", "[", "]", "args", ")"};
-
-            System.out.println("THIS IS TOKENS[" + j + "]: " + tokens[j] + " ");
-            switch(tokens[j]) {
-
-                // Only alters literal array for Main method
-                /*case "PUBLIC":
-                    if (isSubArray(literals, mainMethod)) {
-                        literals[j] = "def main()";
-                    }*/
-                case "LBRACE":
-                    numOfLbrace += 1;
-                    tokens[j] = "SEMI";
-                    // Need someway to update the # of indents
-                    literals[j] = ":\n" + stringMultiply("\t", numOfLbrace);
-                    break;
-                case "INT":
-                case "FLOAT":
-                case "LONG":
-                    literals[j] = "var";
-                    break;
-                // Termination of a statement so we start a new line
-                case "SEMI":
-                    literals[j] = "\n";
-                    break;
-                case "RBRACE":
-                case "EOF":
-                    literals[j] = "";
-                    break;
-            }
-        }
-
-        //Attempting to manipulate spacing
-        for (int k = 0; k < tokens.length - 1; k++) {
-            if (tokens[k+1].equals("LPAREN") || tokens[k + 1].equals("RPAREN")
-                    || tokens[k+1].equals("RBRACK") || tokens[k+1].equals("SEMI")
-                    || tokens[k].equals("LPAREN")) {
-                continue;
-            } else {
-                literals[k] += " ";
-            }
-        }
-        return literals;
-    }
-
-    // Intended to help with indentation
-    // Not working properly as of now
-    public static String stringMultiply(String s, int n){
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < n; i++){
-            sb.append(s);
-        }
-        return sb.toString();
-    }
-
-    // Useful in converting long code literals such as PSVM(S[]A)
-    public static boolean isSubArray (String[] first, String[] second) {
-        int i = 0;
-        int j = 0;
-
-        //Iterate through both arrays simultaneously
-        while (i < first.length && j < second.length) {
-
-            if (first[i] == first[j]) {
-                i++;
-                j++;
-
-                //once the j makes it to the end it must be a sub array
-                if (j == second.length - 1) {
-                    return true;
-                }
-            //Iterate i and reset J
-            } else {
-                i = i - j + 1;
-                j = 0;
-            }
-        }
-        return false;
     }
 
 }
--- SeniorProject/src/MyVisitor.java
+++ SeniorProject/src/MyVisitor.java
@@ -1,10 +1,5 @@
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class MyVisitor extends JavaBaseVisitor<Object>{
-
-
-    private JavaParser.PrimitiveTypeContext ctx;
 
     @Override public Object visitCompilationUnit(JavaParser.CompilationUnitContext ctx) {
         return visitChildren(ctx);
@@ -15,8 +10,289 @@ public class MyVisitor extends JavaBaseVisitor<Object>{
     }
 
     @Override public Object visitPrimitiveType(JavaParser.PrimitiveTypeContext ctx) {
-        System.out.println("\n" + ctx.getText() + " ");
+        //System.out.println("\n" + ctx.getText() + " ");
         return "var";
     }
 
+    @Override public Object visitVariableDeclarator(JavaParser.VariableDeclaratorContext ctx) {
+        //System.out.println("\n" + ctx.getText() + " ");
+        return ctx.getText();
+     }
+
+/*
+    @Override public Object visitImportDeclaration(JavaParser.ImportDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitTypeDeclaration(JavaParser.TypeDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitClassOrInterfaceDeclaration(JavaParser.ClassOrInterfaceDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitClassOrInterfaceModifiers(JavaParser.ClassOrInterfaceModifiersContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitClassOrInterfaceModifier(JavaParser.ClassOrInterfaceModifierContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitModifiers(JavaParser.ModifiersContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitClassDeclaration(JavaParser.ClassDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitNormalClassDeclaration(JavaParser.NormalClassDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitTypeParameters(JavaParser.TypeParametersContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitTypeParameter(JavaParser.TypeParameterContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitTypeBound(JavaParser.TypeBoundContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitEnumDeclaration(JavaParser.EnumDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitEnumBody(JavaParser.EnumBodyContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitEnumConstants(JavaParser.EnumConstantsContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitEnumConstant(JavaParser.EnumConstantContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitEnumBodyDeclarations(JavaParser.EnumBodyDeclarationsContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitInterfaceDeclaration(JavaParser.InterfaceDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitNormalInterfaceDeclaration(JavaParser.NormalInterfaceDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitTypeList(JavaParser.TypeListContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitClassBody(JavaParser.ClassBodyContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitInterfaceBody(JavaParser.InterfaceBodyContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitClassBodyDeclaration(JavaParser.ClassBodyDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitMemberDecl(JavaParser.MemberDeclContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitMemberDeclaration(JavaParser.MemberDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitGenericMethodOrConstructorDecl(JavaParser.GenericMethodOrConstructorDeclContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitGenericMethodOrConstructorRest(JavaParser.GenericMethodOrConstructorRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitMethodDeclaration(JavaParser.MethodDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitFieldDeclaration(JavaParser.FieldDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitInterfaceBodyDeclaration(JavaParser.InterfaceBodyDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitInterfaceMemberDecl(JavaParser.InterfaceMemberDeclContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitInterfaceMethodOrFieldDecl(JavaParser.InterfaceMethodOrFieldDeclContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitInterfaceMethodOrFieldRest(JavaParser.InterfaceMethodOrFieldRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitMethodDeclaratorRest(JavaParser.MethodDeclaratorRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitVoidMethodDeclaratorRest(JavaParser.VoidMethodDeclaratorRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitInterfaceMethodDeclaratorRest(JavaParser.InterfaceMethodDeclaratorRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitInterfaceGenericMethodDecl(JavaParser.InterfaceGenericMethodDeclContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitVoidInterfaceMethodDeclaratorRest(JavaParser.VoidInterfaceMethodDeclaratorRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitConstructorDeclaratorRest(JavaParser.ConstructorDeclaratorRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitConstantDeclarator(JavaParser.ConstantDeclaratorContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitVariableDeclarators(JavaParser.VariableDeclaratorsContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitVariableDeclarator(JavaParser.VariableDeclaratorContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitConstantDeclaratorsRest(JavaParser.ConstantDeclaratorsRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitConstantDeclaratorRest(JavaParser.ConstantDeclaratorRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitVariableDeclaratorId(JavaParser.VariableDeclaratorIdContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitVariableInitializer(JavaParser.VariableInitializerContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitArrayInitializer(JavaParser.ArrayInitializerContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitModifier(JavaParser.ModifierContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitPackageOrTypeName(JavaParser.PackageOrTypeNameContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitEnumConstantName(JavaParser.EnumConstantNameContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitTypeName(JavaParser.TypeNameContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitType(JavaParser.TypeContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitClassOrInterfaceType(JavaParser.ClassOrInterfaceTypeContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitVariableModifier(JavaParser.VariableModifierContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitTypeArguments(JavaParser.TypeArgumentsContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitTypeArgument(JavaParser.TypeArgumentContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitQualifiedNameList(JavaParser.QualifiedNameListContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitFormalParameters(JavaParser.FormalParametersContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitFormalParameterDecls(JavaParser.FormalParameterDeclsContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitFormalParameterDeclsRest(JavaParser.FormalParameterDeclsRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitMethodBody(JavaParser.MethodBodyContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitConstructorBody(JavaParser.ConstructorBodyContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitQualifiedName(JavaParser.QualifiedNameContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitLiteral(JavaParser.LiteralContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAnnotations(JavaParser.AnnotationsContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAnnotation(JavaParser.AnnotationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAnnotationName(JavaParser.AnnotationNameContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitElementValuePairs(JavaParser.ElementValuePairsContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitElementValuePair(JavaParser.ElementValuePairContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitElementValue(JavaParser.ElementValueContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitElementValueArrayInitializer(JavaParser.ElementValueArrayInitializerContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAnnotationTypeDeclaration(JavaParser.AnnotationTypeDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAnnotationTypeBody(JavaParser.AnnotationTypeBodyContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAnnotationTypeElementDeclaration(JavaParser.AnnotationTypeElementDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAnnotationTypeElementRest(JavaParser.AnnotationTypeElementRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAnnotationMethodOrConstantRest(JavaParser.AnnotationMethodOrConstantRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAnnotationMethodRest(JavaParser.AnnotationMethodRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAnnotationConstantRest(JavaParser.AnnotationConstantRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitDefaultValue(JavaParser.DefaultValueContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitBlock(JavaParser.BlockContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitBlockStatement(JavaParser.BlockStatementContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitLocalVariableDeclarationStatement(JavaParser.LocalVariableDeclarationStatementContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitLocalVariableDeclaration(JavaParser.LocalVariableDeclarationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitVariableModifiers(JavaParser.VariableModifiersContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitStatement(JavaParser.StatementContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitCatches(JavaParser.CatchesContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitCatchClause(JavaParser.CatchClauseContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitCatchType(JavaParser.CatchTypeContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitFinallyBlock(JavaParser.FinallyBlockContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitResourceSpecification(JavaParser.ResourceSpecificationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitResources(JavaParser.ResourcesContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitResource(JavaParser.ResourceContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitFormalParameter(JavaParser.FormalParameterContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitSwitchBlockStatementGroups(JavaParser.SwitchBlockStatementGroupsContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitSwitchBlockStatementGroup(JavaParser.SwitchBlockStatementGroupContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitSwitchLabel(JavaParser.SwitchLabelContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitForControl(JavaParser.ForControlContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitForInit(JavaParser.ForInitContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitEnhancedForControl(JavaParser.EnhancedForControlContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitForUpdate(JavaParser.ForUpdateContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitParExpression(JavaParser.ParExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitExpressionList(JavaParser.ExpressionListContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitStatementExpression(JavaParser.StatementExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitConstantExpression(JavaParser.ConstantExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitExpression(JavaParser.ExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAssignmentOperator(JavaParser.AssignmentOperatorContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitConditionalExpression(JavaParser.ConditionalExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitConditionalOrExpression(JavaParser.ConditionalOrExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitConditionalAndExpression(JavaParser.ConditionalAndExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitInclusiveOrExpression(JavaParser.InclusiveOrExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitExclusiveOrExpression(JavaParser.ExclusiveOrExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAndExpression(JavaParser.AndExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitEqualityExpression(JavaParser.EqualityExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitInstanceOfExpression(JavaParser.InstanceOfExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitRelationalExpression(JavaParser.RelationalExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitRelationalOp(JavaParser.RelationalOpContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitShiftExpression(JavaParser.ShiftExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitShiftOp(JavaParser.ShiftOpContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitAdditiveExpression(JavaParser.AdditiveExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitMultiplicativeExpression(JavaParser.MultiplicativeExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitUnaryExpression(JavaParser.UnaryExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitUnaryExpressionNotPlusMinus(JavaParser.UnaryExpressionNotPlusMinusContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitCastExpression(JavaParser.CastExpressionContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitPrimary(JavaParser.PrimaryContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitIdentifierSuffix(JavaParser.IdentifierSuffixContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitCreator(JavaParser.CreatorContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitCreatedName(JavaParser.CreatedNameContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitInnerCreator(JavaParser.InnerCreatorContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitArrayCreatorRest(JavaParser.ArrayCreatorRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitClassCreatorRest(JavaParser.ClassCreatorRestContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitExplicitGenericInvocation(JavaParser.ExplicitGenericInvocationContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitNonWildcardTypeArguments(JavaParser.NonWildcardTypeArgumentsContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitTypeArgumentsOrDiamond(JavaParser.TypeArgumentsOrDiamondContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitNonWildcardTypeArgumentsOrDiamond(JavaParser.NonWildcardTypeArgumentsOrDiamondContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitSelector(JavaParser.SelectorContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitSuperSuffix(JavaParser.SuperSuffixContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitExplicitGenericInvocationSuffix(JavaParser.ExplicitGenericInvocationSuffixContext ctx) { return visitChildren(ctx); }
+
+    @Override public Object visitArguments(JavaParser.ArgumentsContext ctx) { return visitChildren(ctx); } */
 }
+
+
